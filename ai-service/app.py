c8711fc8ac6dde512f510ef0c43685133327c912
Bandapu-Bhuvaneswara-Rao <bhuvaneswararaobandapu@gmail.com>
2026-05-30 12:02:48 +0530

    Added Render deployment files

--- ai-service/app.py
+++ ai-service/app.py
@@ -8,6 +8,13 @@ app = Flask(__name__)
 CORS(app)
 
 
+@app.route("/", methods=["GET"])
+def home():
+    return jsonify({
+        "message": "PDF RAG Bot API Running"
+    })
+
+
 @app.route("/process-pdf", methods=["POST"])
 def process():
 
@@ -55,4 +62,7 @@ def ask():
 
 if __name__ == "__main__":
 
-    app.run(debug=True, port=5000) 
+    app.run(
+        host="0.0.0.0",
+        port=5000
+    )
